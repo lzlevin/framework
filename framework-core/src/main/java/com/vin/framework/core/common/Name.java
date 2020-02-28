c39df06657c3d716791618e7819b37bca95e5395
lzlevin <lzlevin@qq.com>
2020-02-28 23:20:19 +0800

    add:增加名称接口和启用接口

---
+++ framework-core/src/main/java/com/vin/framework/core/common/Name.java
@@ -0,0 +1,24 @@
+package com.vin.framework.core.common;
+
+/**
+ * 具有name属性的
+ *
+ * @author levin
+ * @since 1.0.0
+ */
+public interface Name {
+
+    /**
+     * 获取名称
+     *
+     * @return 名称
+     */
+    String getName();
+
+    /**
+     * 设置名称
+     *
+     * @param name 名称
+     */
+    void setName(String name);
+}

8556465551a10b5571b06223abf04771ed318169
lzlevin <lzlevin@qq.com>
2020-03-29 21:46:32 +0800

    修复错误导入

--- framework-mybatis/src/main/java/com/vf/mybatis/entity/AbstractBaseEntity.java
+++ framework-mybatis/src/main/java/com/vf/mybatis/entity/AbstractBaseEntity.java
@@ -4,7 +4,6 @@ import com.baomidou.mybatisplus.annotation.FieldFill;
 import com.baomidou.mybatisplus.annotation.TableField;
 import com.baomidou.mybatisplus.annotation.TableId;
 import com.baomidou.mybatisplus.annotation.TableLogic;
-import com.vf.core.entity.BaseEntity;
 import lombok.Data;
 
 import java.io.Serializable;

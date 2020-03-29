1bdcf5fbece6ac7fa8f9995d1727e5b9bd9a0b8b
lzlevin <lzlevin@qq.com>
2020-03-29 17:11:16 +0800

    update:更新验证组继承默认验证组

--- framework-validator/src/main/java/com/vf/validate/group/UpdateGroup.java
+++ framework-validator/src/main/java/com/vf/validate/group/UpdateGroup.java
@@ -1,10 +1,12 @@
 package com.vf.validate.group;
 
+import javax.validation.groups.Default;
+
 /**
  * 默认更新验证组
  *
  * @author levin
  * @since 1.0.0
  */
-public interface UpdateGroup {
+public interface UpdateGroup extends Default {
 }
